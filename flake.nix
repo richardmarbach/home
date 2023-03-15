a1b0e855b802b7743f0c8f0e2ec371d616be6a02
Richard Marbach <rmarbach@gmail.com>
2023-03-15 20:20:53 +0100

    Acc nix flake

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "Home manager configuration";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      homeConfigurations.richard = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [
+          ./home.nix
+        ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+        extraSpecialArgs = {
+          user = "richard";
+        };
+      };
+    };
+}
---
+++ home.nix
@@ -0,0 +1,58 @@
+{ config, pkgs, ... }:
+
+let
+  user = "richard";
+in
+{
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "${user}";
+  home.homeDirectory = "/home/${user}";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "23.05";
+
+  home.packages = [
+    pkgs.htop
+    pkgs.neovim
+    pkgs.fd
+    pkgs.ripgrep
+    pkgs.bat
+  ];
+
+  xdg.configFile."alacritty".source = config.lib.file.mkOutOfStoreSymlink ./config/alacritty;
+
+  /* programs.fish = { */
+  /*   enable = true; */
+  /*   plugins = [ */
+  /*     { */
+  /*       name = "plugin-foreign-env"; */
+  /*       src = pkgs.fetchFromGitHub { */
+  /*         owner = "oh-my-fish"; */
+  /*         repo = "plugin-foreign-env"; */
+  /*         rev = "3ee95536106c11073d6ff466c1681cde31001383"; */
+  /*         sha256 = "vyW/X2lLjsieMpP9Wi2bZPjReaZBkqUbkh15zOi8T4Y="; */
+  /*       }; */
+  /*     } */
+  /*   ]; */
+  /* }; */
+  /**/
+  /* home.file.".config/" = { */
+  /*   fish = { */
+  /*     enable = true; */
+  /*     source = config/fish; */
+  /*     recursive = true; */
+  /*   }; */
+  /* }; */
+}
+
